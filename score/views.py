50fd322e1586eeafa29f5f881941f5aee0e3448c
qee <takeen007@gmail.com>
2024-05-14 06:12:04 +0700

    Django form

--- score/views.py
+++ score/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from score.models import Score
-
+from score.forms import ScoreForm
 def index(request):
     context = {}
+    form = ScoreForm()
     scores = Score.objects.all()
     context['scores'] = scores
     context['title'] = 'Home'
+    context['form'] = form
     return render(request, 'index.html', context)
 
 def about(request):
